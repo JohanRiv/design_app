69db88bc4473a790b723079fd98cf06b04e4b75d
JohanRiv <johancamiloandres@hotmail.com>
2022-08-18 23:41:29 -0500

    add adjust on home screen and first widgets

--- design_app/lib/screens/home_screen.dart
+++ design_app/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:design_app/themes/themes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,93 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children: [
+        Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 8, right: 23, top: 20, bottom: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () => {},
+                    icon: const Icon(Icons.menu,
+                        color: PrimaryTheme.primaryLight),
+                  ),
+                  const Text(
+                    'Home',
+                    style: TextStyle(
+                        color: PrimaryTheme.primaryLight,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(100)),
+                    clipBehavior: Clip.antiAlias,
+                    child: const FadeInImage(
+                        fit: BoxFit.cover,
+                        placeholder:
+                            AssetImage('assets/images/gray_background.jpg'),
+                        image: NetworkImage(
+                            'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-774909.jpg&fm=jpg')),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 23, right: 23, top: 20, bottom: 30),
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Good Evening, Elon',
+                        style: TextStyle(
+                            color: PrimaryTheme.primaryLight,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25),
+                      ),
+                      Text(
+                        'Music according to your mood',
+                        style: TextStyle(
+                            color: PrimaryTheme.primaryLight, fontSize: 12),
+                      )
+                    ]),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              // height: 900,
+              decoration: const BoxDecoration(
+                  color: PrimaryTheme.secondaryLight,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.only(top: 20, left: 20, bottom: 20),
+                    child: Text(
+                      'Albums',
+                      style: TextStyle(
+                          color: PrimaryTheme.primaryDark,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
--- design_app/lib/themes/primary_theme.dart
+++ design_app/lib/themes/primary_theme.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class PrimaryTheme {
-  static const Color primary = Color(0x9966FFFF);
-  static const Color secondary = Color(0x6600CCFF);
-  static const Color primaryAux = Color(0x9933CCFF);
-  static const Color primaryLigth = Color(0xFFFFFFFF);
-  static const Color secondaryLigth = Color(0xCCCCCCCC);
-  static const Color primaryDark = Color(0x00000000);
-  static const Color secondaryDark = Color(0x99999999);
+  static const Color primary = Color(0xFF9976CC);
+  static const Color primaryAux = Color(0xFF9969CC);
+  static const Color secondary = Color(0xFF6600CC);
+  static const Color primaryLight = Color(0xFFFFFFFF);
+  static const Color secondaryLight = Color.fromARGB(255, 234, 234, 234);
+  static const Color primaryDark = Color(0xFF000000);
+  static const Color secondaryDark = Color(0xFF999999);
 }
